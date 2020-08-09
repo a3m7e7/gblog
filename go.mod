3c1dada92732541fc1bd1e347e92a0b1f9d792b2
Amir Mohamad Ehsandar <amir.ehsandar@divar.ir>
2020-08-09 17:23:41 +0430

    Implement restful GetAll/ Get Post

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/gorilla/mux v1.7.4
 	go.mongodb.org/mongo-driver v1.4.0
+	google.golang.org/appengine v1.4.0
 	google.golang.org/grpc v1.31.0
 	google.golang.org/protobuf v1.25.0
 )
--- main.go
+++ main.go
@@ -2,40 +2,100 @@ package main
 
 import (
 	"context"
-	"github.com/a3m7e7/gblog/internal/post"
-	postPb "github.com/a3m7e7/gblog/pkg/gblog/post"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 	"log"
-	"net"
+	"net/http"
 )
+
+type IndexResponse struct {
+	Id   string `json:"id" bson:"_id"`
+	Body string `json:"body"`
+}
 
 func main() {
 	ctx := context.Background()
+	mongoClient := getMongoClient(ctx)
 
-	mongoClient := getMongodbClient(ctx)
-	postCollection := mongoClient.Database("gblog").Collection("post")
+	collection := mongoClient.Database("gblog").Collection("post")
 
-	postService := post.New(postCollection)
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			log.Println("could not get all posts")
+			writer.WriteHeader(500)
+			return
+		}
 
-	lis, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+		results := make([]*IndexResponse, 0)
+		if err = cursor.All(ctx, &results); err != nil {
+			writer.WriteHeader(500)
+			return
+		}
 
-	grpcServer := grpc.NewServer()
-	postPb.RegisterPostStorageServer(grpcServer, postService)
+		jsonResp, err := json.Marshal(results)
+		if err != nil {
+			writer.WriteHeader(500)
+			return
+		}
+		_, err = writer.Write(jsonResp)
+		if err != nil {
+			log.Printf("could not write response: %s", request.RequestURI)
+		}
+		return
+	})
 
-	reflection.Register(grpcServer)
+	r.HandleFunc("/{postId}/", func(writer http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		postId, ok := vars["postId"]
+		if !ok {
+			writer.WriteHeader(http.StatusInternalServerError)
+		}
 
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve grpc")
+		docID, err := primitive.ObjectIDFromHex(postId)
+		singleResult := collection.FindOne(ctx, bson.M{"_id": docID})
+		if singleResult.Err() != nil {
+			switch singleResult.Err() {
+			case mongo.ErrNoDocuments:
+				writer.WriteHeader(http.StatusNotFound)
+			default:
+				writer.WriteHeader(http.StatusInternalServerError)
+			}
+			log.Printf("could not get post: %s", postId)
+			return
+		}
+
+		results := IndexResponse{}
+		if err := singleResult.Decode(&results); err != nil {
+			log.Printf("could not decode mongodb response: %s", postId)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		jsonResp, err := json.Marshal(results)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		_, err = writer.Write(jsonResp)
+		if err != nil {
+			log.Printf("could not write response: %s", request.RequestURI)
+		}
+		return
+	})
+
+	http.Handle("/", r)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("failed to listen")
 	}
 }
 
-func getMongodbClient(ctx context.Context) *mongo.Client {
+func getMongoClient(ctx context.Context) *mongo.Client {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		log.Fatalf("could not create mongo client")
